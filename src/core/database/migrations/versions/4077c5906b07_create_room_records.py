f266d78e77e4ab73e4113590eee2336d35b3c2f5
Ромэо <romqaaa1337@gmail.com>
2026-06-07 19:46:16 +0300

    [feat] Заполняет таблицу rooms

---
+++ src/core/database/migrations/versions/4077c5906b07_create_room_records.py
@@ -0,0 +1,64 @@
+"""create_room_records
+
+Revision ID: 4077c5906b07
+Revises: b9468cc1e145
+Create Date: 2026-06-07 19:40:48.889391
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "4077c5906b07"
+down_revision: Union[str, Sequence[str], None] = "b9468cc1e145"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    rooms_table = sa.table(
+        "rooms",
+        sa.column("title", sa.String),
+        sa.column("description", sa.String),
+    )
+
+    op.bulk_insert(
+        rooms_table,
+        [
+            {
+                "title": "Малый зал",
+                "description": "Небольшая переговорная для 2-4 человек.",
+            },
+            {"title": "Большой зал", "description": None},
+            {
+                "title": "Комната отдыха",
+                "description": "Зона для отдыха между встречами.",
+            },
+            {
+                "title": "Серверная",
+                "description": "Техническое помещение (не предназначено для бронирования).",
+            },
+            {
+                "title": "Конференц-зал",
+                "description": "Зал для проведения крупных мероприятий и вебинаров.",
+            },
+        ],
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.execute("""
+            DELETE FROM rooms
+            WHERE title IN (
+                'Малый зал',
+                'Большой зал',
+                'Комната отдыха',
+                'Серверная',
+                'Конференц-зал'
+            )
+        """)
