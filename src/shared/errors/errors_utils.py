78fa0748982b712c7105a7d73251428289b12951
Ромэо <romqaaa1337@gmail.com>
2026-06-12 20:31:30 +0300

    [feat] Делает сообщение об ошибке более информативным

--- src/shared/errors/errors_utils.py
+++ src/shared/errors/errors_utils.py
@@ -6,10 +6,20 @@ from pydantic import ValidationError
 
 def create_message_from_validation_error(
     error: ValidationError | RequestValidationError,
-) -> str:
+) -> dict[str, str]:
     """Функция для создания сообщения об ошибке."""
 
-    return error.errors()[0]["msg"].strip()
+    error_details = error.errors()[0]
+
+    location = error_details["loc"]
+    formatted_location = ".".join(map(str, location))
+
+    return {
+        "type": error_details["type"],
+        "location": formatted_location,
+        "message": error_details["msg"],
+        "input": error_details["input"],
+    }
 
 
 __all__ = ["create_message_from_validation_error"]
