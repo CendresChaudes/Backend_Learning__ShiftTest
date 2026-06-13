87ad127c83ab2c6bf6d5ae35cc7e6dcb97b62b8f
Ромэо <romqaaa1337@gmail.com>
2026-06-13 20:25:30 +0300

    [test] Настраивает int-тесты

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,15 @@
 """Главная конфигурация для тестов."""
 
+import pytest
+
 from src.core.configs.settings import settings
 
 if settings.MODE != "TEST":
     raise Exception(f"Неверное окружение для тестирования: mode={settings.MODE}")
+
+
+@pytest.fixture
+def base_url() -> str:
+    """Возвращает префикс для эндпоинтов API."""
+
+    return "http://localhost:8000/api/v1"
